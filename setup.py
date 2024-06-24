9eda6ee8ccf0fb810ac6bf9e66186b5619346ead
Jared Hall <jared@mononz.com>
2024-06-25 00:57:01 +1000

    remove extra space

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     keywords=['API Alerts', 'push', 'notifications', 'alert', 'monitoring'],
     classifiers=[
         'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License  ',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
     install_requires=['aiohttp', 'dataclasses'],
